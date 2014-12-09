7a5a19ce09ed5e3a934446598ef3ad13a13769ec
Simon Gomez de Kset <simongomezdekset@Simons-MacBook-Air-2.local>
2014-12-09 15:56:43 +0800

    solve merging conflicts

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -7,9 +7,4 @@ class Merchant < ActiveRecord::Base
   geocoded_by :address, :latitude  => :lat, :longitude => :lng   # can also be an IP address
   after_validation :geocode          # auto-fetch coordinates
 
-  def self.search(search)
-    search_condition = "%" + search + "%"
-    find(:all, :conditions => ['category LIKE ?', search_condition])
-  end
-
 end
