eb8d8f79406452ee200f59459f199d34e9efc7dc
Fer Martin <fernandomartinsantana@gmail.com>
2014-12-09 12:33:54 +0800

    Adding categories. Merchant belongs_to category

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,7 @@
 class Merchant < ActiveRecord::Base
+
+  belongs_to :category
+
   reverse_geocoded_by :lat, :lng
   after_validation :reverse_geocode  # auto-fetch address
   geocoded_by :address, :latitude  => :lat, :longitude => :lng   # can also be an IP address
