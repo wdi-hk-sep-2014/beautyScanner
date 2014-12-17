86094f530eee998ab5d84ebc923c4d8fae22cb8d
Simon Gomez de Kset <simongomezdekset@Simons-MacBook-Air-2.local>
2014-12-17 18:39:02 +0800

    create routes for reviews

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :merchants do
     get 'map', on: :member
+    get 'reviews', on: :member
+    put 'update_review', on: :member
   end
 
   resources :categories, :only => [:map, :show] do
