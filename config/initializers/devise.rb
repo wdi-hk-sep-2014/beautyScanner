62412d9dc116843a1489219e582d523a7dc163d4
Simon Gomez de Kset <simongomezdekset@Simons-MacBook-Air-2.local>
2014-12-19 22:01:13 +0800

    change seeds

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -30,7 +30,7 @@ Devise.setup do |config|
   # You can also supply a hash where the value is a boolean determining whether
   # or not authentication should be aborted when the value is not present.
   # config.authentication_keys = [ :email ]
-
+  config.secret_key = '8efbe3fa309f35c797f15c2b7eb6f05e61688829ac2e220848938c0080a357d4b67f6620fe55895b68fed473834977d65be6102b224d2808ed777bd25128f07a'
   # Configure parameters from the request object used for authentication. Each entry
   # given should be a request method and it will automatically be passed to the
   # find_for_authentication method and considered in your model lookup. For instance,
