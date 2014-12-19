ca8441007fa2ef62202d4ef093e6f66a5c261c39
Simon Gomez de Kset <simongomezdekset@Simons-MacBook-Air-2.local>
2014-12-19 21:55:27 +0800

    change geocoder to nominatim

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,8 +1,8 @@
 Geocoder.configure(
   # geocoding options
   :timeout      => 30,           # geocoding service timeout (secs)
-  :lookup       => :google,     # name of geocoding service (symbol)
-  # :lookup       => :nominatim,
+  # :lookup       => :google,     # name of geocoding service (symbol)
+  :lookup       => :nominatim,
   # :language     => :en,         # ISO-639 language code
   # :use_https    => false,       # use HTTPS for lookup requests? (if supported)
   # :http_proxy   => nil,         # HTTP proxy server (user:pass@host:port)
